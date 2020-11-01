76bdc5d341b53c5bad09d342dad45552c246c010
PedroMissaglia <pedro.missaglia@gmail.com>
2020-11-01 01:26:46 -0300

    Alterações de tema

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,15 +3,15 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    PoModule
+    PoModule,
+    MatCardModule
   ],
   exports: [HomeComponent]
 })
